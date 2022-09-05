78ccd4cedf5aa2f8892c6a245656b7473bb8fa47
Caio Cruz <covu@gft.com>
2022-09-05 18:03:58 -0300

    Creating Transactions model and Starting to implement transactions list properly

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/transaction.dart';
+
 main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
@@ -7,14 +9,35 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+
+  final _transactions = [
+    Transaction(
+      id: "t1",
+      title: "New Running Shoes",
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Electricity Bill",
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Car Bill",
+      value: 111.11,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +45,60 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Personal Expenses"),
       ),
-      body: const Center(
-        child: Text("Initial Version"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Card(
+            elevation: 5,
+            child: Text("Chart"),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        tr.date.toString(),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
